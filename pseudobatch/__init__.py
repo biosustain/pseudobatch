eeed6143eccb12dc4f52a5c8b98ff95ffdaba685
viktorht <viktorht@yahoo.dk>
2023-06-06 16:09:17 +0200

    pseudobatch_transform_pandas_by_group function created

--- pseudobatch/__init__.py
+++ pseudobatch/__init__.py
@@ -1,7 +1,7 @@
 import shutil
 import warnings
 from pathlib import Path
-from .data_correction import pseudobatch_transform_multiple, pseudobatch_transform, pseudobatch_transform_pandas, accumulated_dilution_factor, convert_volumetric_rates_from_pseudo_to_real
+from .data_correction import pseudobatch_transform_multiple, pseudobatch_transform, pseudobatch_transform_pandas, accumulated_dilution_factor, convert_volumetric_rates_from_pseudo_to_real, pseudobatch_transform_pandas_by_group
 import cmdstanpy
 
 STAN_FILES_FOLDER = Path(__file__).parent / "stan"
